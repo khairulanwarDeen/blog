ffa7a929c577b880decb62387ecf60180fd58075
khairulanwarDeen <khairulanwarbinsharifuddeen@gmail.com>
2022-04-14 17:25:26 +0800

    emitting and receiving events

--- client/src/index.js
+++ client/src/index.js
@@ -6,3 +6,15 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
 )
+
+// ****YOU WILL NEED A CATCH BLOCK TO EVERY REQUEST OF THE event-bus/index.js
+// axios.post('http://localhost:4000/events', event).catch((err) => {
+//     console.log(err.message);
+//   });
+//   axios.post('http://localhost:4001/events', event).catch((err) => {
+//     console.log(err.message);
+//   });
+//   axios.post('http://localhost:4002/events', event).catch((err) => {
+//     console.log(err.message);
+//   });
+//   res.send({ status: 'OK' });
