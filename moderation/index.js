fcbba3662eb8170d3db49ba0ef439ed2ac7cb43b
khairulanwarDeen <khairulanwarbinsharifuddeen@gmail.com>
2022-04-25 17:01:13 +0800

    implement docker

--- moderation/index.js
+++ moderation/index.js
@@ -5,10 +5,7 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-//This is where we receive events posted here
-app.post('/events', async (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = async (type, data) => {
     if (type === 'CommentCreated'){
         const status = data.content.includes('orange')? 'rejected' : 'approved';
     
@@ -22,10 +19,29 @@ app.post('/events', async (req, res) => {
             }
         })
     }
+}
+
+//This is where we receive events posted here
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 });
 
-app.listen(4003, () => {
+app.listen(4003, async () => {
     console.log("Listening on 4003");
+
+    try {
+        const res = await axios.get('http://localhost:4005/events');
+
+        for (let event of res.data) {
+            console.log("Processing event:", event.type);
+
+            handleEvent(event.type, event.data)
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
 })
