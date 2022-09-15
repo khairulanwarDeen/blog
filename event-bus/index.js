086e0dfe19bfd5270d8c9ecc7259add135986ba7
khairulanwarDeen <khairulanwarbinsharifuddeen@gmail.com>
2022-09-15 23:00:18 +0800

    created k8 cluster for all services

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,16 +12,16 @@ app.post("/events", (req, res) => {
 
   events.push(event);
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: "OK" });
@@ -32,5 +32,5 @@ app.get("/events", (req, res) => {
 });
 
 app.listen(4005, () => {
-  console.log("Listening on 4005");
+  console.log("http://posts-clusterip-srv:4000/events Listening on 4005");
 });
